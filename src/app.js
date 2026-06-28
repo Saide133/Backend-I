ad9be977139b7222ce03df3413f51633b9522e56
Lucia <lucianunez995@gmail.com>
2026-06-28 19:48:16 -0300

    feat: estructura completa del proyecto

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import { env } from './config/env.js'
 
 const app = express()
 
@@ -9,24 +8,4 @@ app.get('/', (req, res) => {
     res.send('Servidor funcionando')
 })
 
-app.get('/turnos', (req, res) => {
-    res.json({ mensaje: 'Lista de turnos' })
-})
-
-app.post('/turnos', (req, res) => {
-    const datos = req.body
-    res.json({ mensaje: 'Turno creado', datos })
-})
-
-app.put('/turnos/123', (req, res) => {
-    const datos = req.body
-    res.json({ mensaje: 'Turno 123 modificado', datos })
-})
-
-app.delete('/turnos/123', (req, res) => {
-    res.json({ mensaje: 'Turno 123 eliminado' })
-})
-
-app.listen(env.PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${env.PORT}`)
-})
+export default app
